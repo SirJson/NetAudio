1cd46e2fae65aa9670f0b46eb989a25065eb1450
Raffael Zica <raffael@pixeltastic.de>
2019-11-10 10:21:47 +0100

    Initial commit

---
+++ audio_renderer/src/main.rs
@@ -0,0 +1,101 @@
+use byteorder::{ByteOrder, NetworkEndian};
+use rodio::Sink;
+use rodio::{Sample, Source};
+use std::collections::VecDeque;
+use std::net::UdpSocket;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::time::Duration;
+
+type SampleType = f32;
+const UDP_BUFFER_SIZE: usize = 65507;
+const NETWORK_ADDRESS: &str = "0.0.0.0:11331";
+
+struct AudioStream {
+    data: VecDeque<SampleType>,
+    receiver: Receiver<Vec<SampleType>>,
+}
+
+impl AudioStream {
+    fn new(receiver: Receiver<Vec<SampleType>>) -> Self {
+        AudioStream {
+            data: VecDeque::new(),
+            receiver,
+        }
+    }
+}
+
+impl Iterator for AudioStream {
+    type Item = SampleType;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Ok(foo) = self.receiver.try_recv() {
+            self.data.extend(&foo);
+        }
+        let out = self.data.pop_front();
+        if out == None {
+            Some(Self::Item::zero_value())
+        } else {
+            out
+        }
+    }
+}
+
+impl Source for AudioStream {
+    fn current_frame_len(&self) -> Option<usize> {
+        None
+    }
+
+    fn channels(&self) -> u16 {
+        2
+    }
+
+    fn sample_rate(&self) -> u32 {
+        48000
+    }
+
+    fn total_duration(&self) -> Option<Duration> {
+        None
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    println!("NetAudio Server v1.0");
+    let (tx, rx): (Sender<Vec<SampleType>>, Receiver<Vec<SampleType>>) = mpsc::channel();
+    let mut buffer = Box::new([0; UDP_BUFFER_SIZE]);
+
+    let device = rodio::default_output_device().expect("Failed to select default output device");
+    println!("Output Device: {}",device.name());
+
+    let sink = Sink::new(&device);
+    let stream = AudioStream::new(rx);
+
+    println!("Binding to address: {}",NETWORK_ADDRESS);
+    let socket = UdpSocket::bind(NETWORK_ADDRESS).expect("Failed to bind network address");
+
+    sink.append(stream);
+    sink.play();
+
+    println!("Starting main loop!");
+    loop {
+        let bytes_recv = match socket.recv_from(&mut *buffer) {
+            Ok(n) => n.0,
+            Err(ref e) if e.kind() == std::io::ErrorKind::WouldBlock => 0,
+            Err(e) => panic!("Encountered Network IO error: {}", e),
+        };
+        if bytes_recv > 0 {
+            let source = &mut buffer[..bytes_recv];
+            if bytes_recv % 4 != 0 {
+                eprintln!("Bytes received is not a multiple of 4! Skipping packet...");
+                continue;
+            }
+            let mut target: Vec<f32> = vec![0.0; bytes_recv / 4];
+            NetworkEndian::read_f32_into(&source, &mut target);
+            if let Err(e) = tx.send(target) {
+                panic!("Internal error: {}", e);
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ audio_source/src/main.rs
@@ -0,0 +1,63 @@
+use byteorder::{ByteOrder, NetworkEndian};
+use cpal::traits::{DeviceTrait, EventLoopTrait, HostTrait};
+use std::net::UdpSocket;
+
+fn main() -> std::io::Result<()> {
+    println!("Connecting..");
+    let socket = UdpSocket::bind("127.0.0.1:11330").expect("couldn't bind to address");
+    socket
+        .connect("127.0.0.1:11331")
+        .expect("connect function failed");
+
+    println!("Start stream..");
+
+    // Use the default host for working with audio devices.
+    let host = cpal::default_host();
+
+    // Setup the default input device and stream with the default input format.
+    let device = host
+        .default_output_device()
+        .expect("Failed to get default output device");
+    println!("Default input device: {}", device.name().unwrap());
+    let format = device
+        .default_output_format()
+        .expect("Failed to get default output format");
+    println!("Default input format: {:?}", format);
+    let event_loop = host.event_loop();
+    let stream_id = event_loop.build_input_stream(&device, &format).unwrap();
+    event_loop.play_stream(stream_id).unwrap();
+    event_loop.run(move |id, event| {
+        let data = match event {
+            Ok(data) => data,
+            Err(err) => {
+                eprintln!("an error occurred on stream {:?}: {}", id, err);
+                return;
+            }
+        };
+        // Otherwise write to the wav writer.
+        match data {
+            cpal::StreamData::Input {
+                buffer: cpal::UnknownTypeInputBuffer::U16(buffer),
+            } => {
+                let mut pkg = vec![0; buffer.len() * 2];
+                NetworkEndian::write_u16_into(&buffer, pkg.as_mut_slice());
+                socket.send(&pkg).expect("Failed to send package");
+            }
+            cpal::StreamData::Input {
+                buffer: cpal::UnknownTypeInputBuffer::I16(buffer),
+            } => {
+                let mut pkg = vec![0; buffer.len() * 2];
+                NetworkEndian::write_i16_into(&buffer, pkg.as_mut_slice());
+                socket.send(&pkg).expect("Failed to send package");
+            }
+            cpal::StreamData::Input {
+                buffer: cpal::UnknownTypeInputBuffer::F32(buffer),
+            } => {
+                let mut pkg = vec![0; buffer.len() * 4];
+                NetworkEndian::write_f32_into(&buffer, pkg.as_mut_slice());
+                socket.send(&pkg).expect("Failed to send package");
+            }
+            _ => (),
+        }
+    });
+}
